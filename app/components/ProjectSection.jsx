21cbe9114627ed3aa3a63592e11f6ba797c38304
pablogarciadegregorio <bleraspa@gmail.com>
2024-02-23 23:17:23 +0100

    corrected reloj1 name

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -47,7 +47,7 @@ const projectsData = [
     id: 5,
     title: "Merchandising",
     description: "Diseño de productos para bandas",
-    image: "/images/projects/reloj1.jpg",
+    image: "public/images/projects/Reloj1.jpg",
     tag: ["Todos", "Diseño"],
     // gitUrl: "/",
     previewUrl: "/",
